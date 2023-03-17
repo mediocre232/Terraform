57d3c6097e4fdff68887e2aa428256cf4fa0778b
suhail <65529400+mediocre232@users.noreply.github.com>
2023-03-16 22:38:32 -0400

    Update workspace.tf

--- workspace.tf
+++ workspace.tf
@@ -5,7 +5,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "myec2" {
-   ami = "ami-082b5a644766e0e6f"
+   ami = "ami-0d81306eddc614a45"
    instance_type = lookup(var.instance_type,terraform.workspace)
 }
 
